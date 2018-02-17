03b05af2a2e518aa77aa2b39727032637d25f78a
slk000 <me@lo-li.net>
2018-02-17 22:21:35 +0800

    first unit test with unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
+class NewVisitorTest(unittest.TestCase):
 
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 伊迪丝听说有一个很酷的在线待办事项应用
+        # 她去看了这个应用的首页
+        self.browser.get('http://localhost:8000')
+
+        # 她注意到网页的标题和头部都包含“ To-Do ”这个词
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
