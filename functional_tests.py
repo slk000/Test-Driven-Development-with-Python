9286840e94c32438a2e38f7e509ab49988850d31
slk000 <me@lo-li.net>
2018-02-17 23:34:58 +0800

    Fix typo, Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -18,11 +18,11 @@ class NewVisitorTest(unittest.TestCase):
 
         # 她注意到网页的标题和头部都包含“ To-Do ”这个词
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').header_text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         
         # 应用邀请她输入一个待办事项
-        inputbox = self.browser.find_element_by_tag_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -30,15 +30,16 @@ class NewVisitorTest(unittest.TestCase):
 
         # 她在一个文本框中输入了“ Buy peacock feathers ”( 购买孔雀羽毛)
         # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
-        input.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
         # 她按回车键后,页面更新了
         # 待办事项表格中显示了“ 1: Buy peacock feathers ”
-        input.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_tag_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # 页面中又显示了一个文本框,可以输入其他的待办事项
