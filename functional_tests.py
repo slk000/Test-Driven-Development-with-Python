d647b04e5e24a84262d5ca7c33842674f87a746b
slk000 <me@lo-li.net>
2018-02-20 16:24:49 +0800

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -51,7 +51,7 @@ class NewVisitorTest(unittest.TestCase):
         # 她输入了“ Use peacock feathers to make a fly” ( 使用孔雀羽毛做假蝇)
         # 伊迪丝做事很有条理
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feather to make a fly')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新,清单中显示了这两个待办事项
