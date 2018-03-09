ae8dc3740dc1bdea16be108107a21e1e535dad0b
slk000 <me@lo-li.net>
2018-03-09 22:49:37 +0800

    new_list view

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from lists import views
+from lists import views as ListsViews
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home'),
-    url(r'lists/the-only-list-in-the-world/$', views.view_list, name='view_list')
+    url(r'^$', ListsViews.home_page, name='home'),
+    url(r'lists/the-only-list-in-the-world/$', ListsViews.view_list, name='view_list'),
+    url(r'lists/new$', ListsViews.new_list, name='new_list'),
     # url(r'^admin/', admin.site.urls),
 ]
