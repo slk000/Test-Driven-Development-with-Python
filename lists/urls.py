be2a12d9d36fa36012ebcdc3b918c54918d8cdd1
slk000 <me@lo-li.net>
2018-03-12 13:32:25 +0800

    refacte urls

--- lists/urls.py
+++ lists/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 from lists import views as ListsViews
 
 urlpatterns = [
-    url(r'^$', ListsViews.home_page, name='home'),
-    url(r'lists/', include('lists.urls')),
-    # url(r'^admin/', admin.site.urls),
+    url(r'(\d+)/$', ListsViews.view_list, name='view_list'),
+    url(r'(\d+)/add_item$', ListsViews.add_item, name='add_item'),
+    url(r'new$', ListsViews.new_list, name='new_list'),
 ]
