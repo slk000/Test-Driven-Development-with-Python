0b7b3b7fd620df09be18e729f863bbb60c7dae13
slk000 <me@lo-li.net>
2018-03-13 15:42:23 +0800

    Split out unit test into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -10,7 +10,7 @@ from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
-
+    
     def remove_csrf(self, html):
         import re
         csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
@@ -34,34 +34,6 @@ class HomePageTest(TestCase):
             self.remove_csrf(expected_html),
         )
 
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
     def test_passes_correct_list_to_template(self):
