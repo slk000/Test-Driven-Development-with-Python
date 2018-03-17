c7f5cf1868689e02080aac87cf9cabd55562fab5
slk000 <me@lo-li.net>
2018-03-17 21:42:32 +0800

    use form in all views + refactor same tests. back to working state now

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,11 +3,15 @@ from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
-    
+    def remove_inputbox_required_attr(self):
+        js = "document.getElementById('id_text').removeAttribute('required')"
+        self.browser.execute_script(js)
+
     def test_cannot_add_empty_list_items(self):
         # 伊迪丝访问首页,不小心提交了一个空待办事项
         # 输入框中没输入内容,她就按下了回车键
         self.browser.get(self.server_url)
+        self.remove_inputbox_required_attr()
         self.get_item_input_box().send_keys('\n')
 
         # 首页刷新了,显示一个错误消息
@@ -20,6 +24,7 @@ class ItemValidationTest(FunctionalTest):
         self.check_for_row_in_list_table('1: Buy milk')
 
         # 她有点儿调皮,又提交了一个空待办事项
+        self.remove_inputbox_required_attr()
         self.get_item_input_box().send_keys('\n')
 
         # 在列表页面她看到了一个类似的错误消息
